3f65bb4011affd6fc918aabef6b24c256ae7afee
Christoph Witzko <github@christophwitzko.com>
2015-02-04 22:38:15 +0100

    feat: implemented server

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 'use strict'
+
+var fs = require('fs')
+
+var bodyParser = require('body-parser')
+var defaults = require('lodash.defaults')
+var dot = require('dot')
+var express = require('express')
+
+var releaseStore = {}
+
+var responseTemplate = dot.template(fs.readFileSync(__dirname + '/response.dot'))
+
+var defaultReleaseValues = {
+  tag_name: 'v1.0.0',
+  target_commitish: 'master',
+  name: 'v1.0.0',
+  body: '',
+  draft: false,
+  prerelease: false
+}
+
+var app = express()
+
+app.use(bodyParser.json())
+app.use(function (req, res, next) {
+  res.set('X-RateLimit-Limit', '5000')
+  res.set('X-RateLimit-Remaining', '4999')
+  next()
+})
+
+app.post('/repos/:owner/:repo/releases', function (req, res) {
+  var owner = req.params.owner
+  var repo = req.params.repo
+  var key = owner + ':' + repo
+  res.set('Location', 'https://api.github.com/repos/' + owner + '/' + repo + '/releases/1')
+  defaults(req.body, defaultReleaseValues, {owner: owner, repo: repo})
+  console.log('setting ->', key)
+  releaseStore[key] = req.body
+  res.status(201).send(responseTemplate(req.body))
+})
+
+app.get('/repos/:owner/:repo/releases/:id', function (req, res) {
+  var owner = req.params.owner
+  var repo = req.params.repo
+  var key = owner + ':' + repo
+  console.log('getting ->', key)
+  if (!releaseStore[owner + ':' + repo]) return res.status(404).end()
+  res.status(200).send(responseTemplate(releaseStore[owner + ':' + repo]))
+})
+
+app.listen(4343, function () {
+  console.log('listening on port 4343')
+})
